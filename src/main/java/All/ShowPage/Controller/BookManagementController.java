94d83d2a2ec44aa0f9cbb6ff84bbafa05f68668c
DuZiHao <446397151@qq.com>
2021-11-27 09:15:16 +0800

    本地

--- src/main/java/All/ShowPage/Controller/BookManagementController.java
+++ src/main/java/All/ShowPage/Controller/BookManagementController.java
@@ -1,28 +1,30 @@
 package All.ShowPage.Controller;
 
+import All.Model.BookInformation;
 import All.ShowPage.Dao.ShowBookByBname;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class BookManagementController {
     @RequestMapping(value = "BookManagementController")
     public String BookManagementControl(){
-        return "test2";
+        return "test1";
     }
-    @RequestMapping(value = "BookManagementShow")
+    @RequestMapping(value = "TeamWorkShowController")
+    public String TeamWorkShowController(){
+        return "tushu";
+    }
+    @RequestMapping(value = "BookManagementShow", method = RequestMethod.POST)
     @ResponseBody
-    public Map<String,Object> BookManagementShow(){
+    public List Show(){
         ShowBookByBname show=new ShowBookByBname();
-        Map<String,Object> map=new HashMap<>();
-        map.put("BookList",show.GetList());
-        return map;
+        return show.GetList();
     }
 }
